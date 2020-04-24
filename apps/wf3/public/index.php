230751e939404d2550ed18784431b303121ba27e
Hadrien <delphin.hadrien@gmail.com>
2020-04-24 15:15:40 +0200

    exercice élèves / sections

--- apps/wf3/public/index.php
+++ apps/wf3/public/index.php
@@ -2,37 +2,59 @@
 
 use App\DevTools;
 use App\LibsLoader;
-use App\Models\Voiture;
+
 
 $loader = require '../vendor/autoload.php';
 
 $libsLoader = new LibsLoader();
 $libsLoader->loadLibraries();
 
-$voitures = [
-  ['model' => '208',
-      'mark' => 'Peugeot',
-      'price' => 10000,
-      'TVA' => 20
-  ],
-    [
-        'model' => 'duster',
-        'mark' => 'Dacia',
-        'price' => 15000,
-        'TVA' => 20
-    ]
-];
-
-$carsObjs = [];
-
 $tools = new DevTools();
 
+$students = [
 
-foreach ($voitures as $voiture) {
-    $carObj = new Voiture();
-    $carObj->hydrateCar($voiture['mark'], $voiture['model'], $voiture['price'], $voiture['TVA']);
-    array_push($carsObjs, $carObj);
-}
+    [
+        "name" => "Hadrien",
+        "age" => 31,
+        "section" => "CE1"
+    ],
+    [
+        "name" => "Julien",
+        "age" => 33,
+        "section" => "CE1"
+    ],
+    [
+        "name" => "Theo",
+        "age" => 20,
+        "section" => "CP"
+    ],
+    [
+        "name" => "Samantha",
+        "age" => 30,
+        "section" => "CE2"
+    ],
+    [
+        "name" => "Emilie",
+        "age" => 24,
+        "section" => "CE2"
+    ],
+    [
+        "name" => "Vladimir",
+        "age" => 64,
+        "section" => "CM2"
+    ],
+    [
+        "name" => "Phillipe",
+        "age" => 47,
+        "section" => "CM2"
+    ]
 
-$tools->prettyVarDump($carsObjs);
+];
 
+
+/*
+ * Faire un algo qui va créer un école en fonction des élèves du tableau : Si un élève est dans une section et que cette section n'existe pas encore, la créer,
+ * sinon ajouter cet élève a la section
+ * chaque élève doit êre un objet
+ * chaque section est un objet qui contient un tabeau d'objets élèves
+ */
